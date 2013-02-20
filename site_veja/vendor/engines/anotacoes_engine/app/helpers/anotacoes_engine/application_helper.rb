442509dcb7b285587e2645fc7f5badc47009c8d5
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2013-02-20 14:18:55 -0300

    Permitindo a renderização customizada de comentários, via um bloco (que recebe resource_uri, comments e options como variáveis de bloco). Ex:
      = render_comments @article.id, show_form: true do |resource_uri, comments, options|
        - if comments.any?
          %table(border=1 cellpadding=1 cellspacing=0)
            - comments.each do |comment|
              %tr
                %td= comment.body
                %td= comment.created_at
        - else
          %p Nenhum comentário (seja o primeiro a comentar este artigo)

--- site_veja/vendor/engines/anotacoes_engine/app/helpers/anotacoes_engine/application_helper.rb
+++ site_veja/vendor/engines/anotacoes_engine/app/helpers/anotacoes_engine/application_helper.rb
@@ -1,11 +1,17 @@
 module AnotacoesEngine
   module ApplicationHelper
-    def render_comments(resource_uri, options = {})
+    def render_comments(resource_uri, options = {}, &block)
       options.reverse_merge!(
         show_form: true
       )
       
-      render partial: '/comments', locals: { comments: Comment.search(resource_uri), resource_uri: resource_uri, show_form: options[:show_form] }
+      comments = Comment.search(resource_uri)
+      
+      if block
+        capture resource_uri, comments, options, &block
+      else
+        render partial: '/comments', locals: { resource_uri: resource_uri, comments: comments, show_form: options[:show_form] }
+      end
     end
     
     def anotacoes_domain_alive?
